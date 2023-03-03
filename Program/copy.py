0429265347c3c8c9555e50239f0891da10f6f4f2
Literat777 <Literat777@users.noreply.github.com>
2023-03-03 11:39:15 +0300

    feat: add CopyText

--- Program/copy.py
+++ Program/copy.py
@@ -1,10 +1,3 @@
-def onCopy(self, event):
-        //Копировать в буфер обмена
-		
-        self.dataObj = wx.TextDataObject()
-        self.dataObj.SetText(self.text.GetValue())
-        if wx.TheClipboard.Open():
-            wx.TheClipboard.SetData(self.dataObj)
-            wx.TheClipboard.Close()
-        else:
-            wx.MessageBox("Unable to open the clipboard", "Error")
+def copy():
+    t_input.clipboard_clear()  # Очистить буфер обмена
+    t_input.clipboard_append(t_input.get(1.0, END))  # Скопировать текст в буфер обмена
